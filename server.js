337e12b080f307ebd85679d87d700af47187252b
mconiaris <mconiaris@gmail.com>
2021-07-13 12:23:19 -0400

    Added public folder to server.js.

--- server.js
+++ server.js
@@ -8,5 +8,7 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '/index.html'));
 });
 
+app.use(express.static('public'));
+
 app.listen(port);
 console.log('Server started at http://localhost:' + port);
